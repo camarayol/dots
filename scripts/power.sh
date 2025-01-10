d3e853780fa515c19bd52ec278540e0d0e4997e6
camarayol <camarayol@outlook.com>
2025-01-10 13:56:10 +0800

    chore(fish,mpd,rofi,waybar,zellij): update configurations

---
+++ scripts/power.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+declare -A power_menus=(
+    ["  lock"]="hyprlock"
+    ["  suspend"]="systemctl suspend"
+    ["  logout"]="hyprctl dispatch exit 1"
+    ["  reboot"]="systemctl reboot"
+    ["  shutdown"]="systemctl poweroff"
+)
+
+default_rasi="inputbar { enabled: false; } listview { scrollbar: false; } window { width: 400px; }"
+command=$(printf "%s\n" "${!power_menus[@]}" | rofi -dmenu -theme-str "$default_rasi window { height: 300px; } listview { lines: 5; }")
+
+if [[ -n $command ]]; then
+    confirm=$(printf "Yes\nNo" | rofi -dmenu -theme-str "$default_rasi window { height: 160px; } listview { lines: 2; }")
+
+    if [[ -n $confirm && $confirm == "Yes" ]]; then
+        ${power_menus[$command]}
+    fi
+fi
+
