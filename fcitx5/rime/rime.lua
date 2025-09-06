36f3a939d09edfaba4bcf2a61d1d2e9377daffef
camarayol <camarayol@outlook.com>
2025-09-06 22:19:52 +0800

    chore(rime,ghostty,mpd): some updates
    1

--- fcitx5/rime/rime.lua
+++ fcitx5/rime/rime.lua
@@ -22,10 +22,6 @@ number_translator = require("number_translator")
 
 -- filters:
 
--- 错音错字提示
--- 关闭此 Lua 时，同时需要关闭 translator/spelling_hints，否则 comment 里都是拼音
-corrector = require("corrector")
-
 -- v 模式 symbols 优先（全拼）
 v_filter = require("v_filter")
 
@@ -54,27 +50,12 @@ cn_en_spacer = require("cn_en_spacer")
 -- 在 engine/filters 的倒数第二个位置，增加 - lua_filter@en_spacer
 en_spacer = require("en_spacer")
 
--- 九宫格，将输入框的数字转为对应的拼音或英文，iRime 用，Hamster 不需要。
--- 在 engine/filters 增加 - lua_filter@t9_preedit
-t9_preedit = require("t9_preedit")
-
 -- 根据是否在用户词典，在 comment 上加上一个星号 *
 -- 在 engine/filters 增加 - lua_filter@is_in_user_dict
 -- 在方案里写配置项：
 -- is_in_user_dict: true     为输入过的内容加星号
 -- is_in_user_dict: false    为未输入过的内容加星号
 is_in_user_dict = require("is_in_user_dict")
-
--- 词条隐藏、降频
--- 在 engine/processors 增加 - lua_processor@cold_word_drop_processor
--- 在 engine/filters 增加 - lua_filter@cold_word_drop_filter
--- 在 key_binder 增加快捷键：
--- turn_down_cand: "Control+j"  # 匹配当前输入码后隐藏指定的候选字词 或候选词条放到第四候选位置
--- drop_cand: "Control+d"       # 强制删词, 无视输入的编码
--- get_record_filername() 函数中仅支持了 Windows、macOS、Linux
-cold_word_drop_processor = require("cold_word_drop.processor")
-cold_word_drop_filter = require("cold_word_drop.filter")
-
 
 -- 暴力 GC
 -- 详情 https://github.com/hchunhui/librime-lua/issues/307
