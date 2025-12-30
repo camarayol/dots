e0f413126491c75343f41a34a9f1e667ec261b65
camarayol <camarayol@outlook.com>
2025-12-30 14:01:34 +0800

    chore(nvim): simplify markdown autolist

--- nvim/lua/plugins/smart-pairs.lua
+++ nvim/lua/plugins/smart-pairs.lua
@@ -38,32 +38,7 @@ return {
                     text_bracket = { one = { strategy = "leave_one_start", } },
                 }
             },
-            enter = {
-                enable_mapping = true,
-                enable_cond    = true,
-                before_hook    = function()
-                    -- https://github.com/gaoDean/autolist.nvim/issues/77
-                    local filetype = vim.api.nvim_get_option_value("filetype", { buf = 0 })
-                    if filetype == "txt" or filetype == "markdown" then
-                        vim.uv.new_timer():start(0, 0, vim.schedule_wrap(function()
-                            require("autolist").new_bullet()
-                        end))
-                    end
-                end
-            },
-        }
-
-        MiniDeps.lazy {
-            source = "https://github.com/gaoDean/autolist.nvim",
-            config = function()
-                Core.createAutoCommand("InsertEnter", { "*.txt", "*.md" }, function()
-                    require("autolist").setup {}
-                    Core.setKeyMaps {
-                        { "n", "o", "o<Cmd>AutolistNewBullet<CR>" },
-                        { "n", "O", "O<Cmd>AutolistNewBulletBefore<CR>" },
-                    }
-                end)
-            end
+            enter = { enable_mapping = true, enable_cond = true }
         }
     end
 }
