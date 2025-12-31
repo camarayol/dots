8931c0febc9dd7c4ea7131c127574e3ec5c8323d
camarayol <camarayol@outlook.com>
2025-12-31 15:11:58 +0800

    chore(nvim): update 'nvim-treesitter'

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,26 +2,14 @@ return {
     source = "https://github.com/nvim-treesitter/nvim-treesitter",
     hooks = { post_install = function() vim.cmd("TSUpdate") end },
     config = function()
-        require("nvim-treesitter.configs").setup {
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-CR>"
-                }
-            }
-        }
+        require("nvim-treesitter").setup {}
+
         vim.treesitter.language.register("json5", "json")
 
-        MiniDeps.lazy {
-            source = "https://github.com/nvim-treesitter/playground",
-            config = function()
-                Core.setKeyMaps { { "n", "<F2>", "<Cmd>TSHighlightCapturesUnderCursor<CR>" } }
-            end
-        }
+        Core.createAutoCommand("FileType", {
+            "rust", "cpp", "json", "sql", "markdown", "markdown_inline", "typst"
+        }, function() vim.treesitter.start() end)
+
+        Core.setKeyMaps { { "n", "<F2>", "<Cmd>Inspect<CR>" } }
     end
 }
