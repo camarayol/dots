a2a18058b0e013b54a7e30c5c9cd72d64feecae5
camarayol <camarayol@outlook.com>
2026-01-03 20:58:02 +0800

    chore(nvim): update 'nvim-treesitter'

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,14 +2,16 @@ return {
     source = "https://github.com/nvim-treesitter/nvim-treesitter",
     hooks = { post_install = function() vim.cmd("TSUpdate") end },
     config = function()
-        require("nvim-treesitter").setup {}
+        Core.setKeyMaps { { "n", "<F2>", "<Cmd>Inspect<CR>" } }
+
+        Core.createAutoCommand({ "BufReadPost", "BufNewFile" }, nil, function()
+            if vim.bo.filetype ~= "" then
+                pcall(vim.treesitter.start)
+            end
+        end)
 
         vim.treesitter.language.register("json5", "json")
 
-        Core.createAutoCommand("FileType", {
-            "rust", "cpp", "json", "sql", "markdown", "markdown_inline", "typst"
-        }, function() vim.treesitter.start() end)
-
-        Core.setKeyMaps { { "n", "<F2>", "<Cmd>Inspect<CR>" } }
+        require("nvim-treesitter").setup {}
     end
 }
