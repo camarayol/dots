45979be9da23a90b3621afe4ce776cb9080f06ff
camarayol <camarayol@outlook.com>
2026-03-12 22:17:18 +0800

    feat(nvim): update 'copilot.nvim' configurations

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,6 +1,20 @@
 return {
     source = 'https://github.com/zbirenbaum/copilot.lua',
     config = function()
-        require('copilot').setup {}
+        require('copilot').setup {
+            panel = { enabled = false },
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                debounce = 150,
+                keymap = {
+                    accept = '<M-l>',
+                    accept_word = false,
+                    accept_line = false,
+                    next = '<M-]>',
+                    prev = '<M-[>',
+                },
+            },
+        }
     end
 }
